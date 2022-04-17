7326d0d7e1b422742caddd5741067b58bb9212ba
root_who <joaopaulo_gonzaga@outlook.com>
2022-04-17 19:39:11 -0300

    exercicio 1

---
+++ exercicio1.js
@@ -0,0 +1,23 @@
+// 1. Escreva funções que implementam as quatro operações aritméticas. Todas elas devem
+// receber dois valores numéricos como parâmetro e devolver o valor de sua respectiva
+// operação. Faça a implementação usando a palavra function para todas elas.
+// Programadores: João Paulo Gonzaga e Cleyffson Cardoso
+
+
+function adicicao(num1, num2){
+    return num1 + num2;
+}
+
+function subtracao(num1, num2){
+    return num1 - num2;
+}
+
+function divisao(num1, num2){
+    return num1 / num2;
+}
+
+function multiplicacao(num1, num2){
+    return num1 * num2;
+}
+
+
